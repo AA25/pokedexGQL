12d7a22877a2f5fdaf029c61c3cceddeff27dcf6
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2021-03-19 17:51:12 +0000

    Added estint plugins for react and react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-  root: true,
-  extends: [
-      '@react-native-community',
-      // https://github.com/facebook/react-native/blob/master/packages/eslint-config-react-native-community/index.js
-      'plugin:prettier/recommended',
-  ]
+    root: true,
+    extends: [
+        "@react-native-community",
+        // https://github.com/facebook/react-native/blob/master/packages/eslint-config-react-native-community/index.js
+        "plugin:prettier/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+    ],
+    plugins: [
+        "react",
+        "react-hooks",
+    ],
+    rules: {
+        "react/prop-types": "off" // TODO: Understand later
+    },
 };

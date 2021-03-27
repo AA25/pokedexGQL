d973e5d6f0c3045484ca800de07812fc5239ebee
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2021-03-27 14:36:35 +0000

    Added themed splash screen
    Still needs refactoring but

--- src/containers/SplashScreen.tsx
+++ src/containers/SplashScreen.tsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { styles } from "../styles/styles";
-import { Button, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View} from "react-native";
 import { Navigation } from "react-native-navigation";
 import { mainRoot } from "../navigation/nagivationRoots";
+import { PokedexHeader } from "../components/PokedexHeader";
+import { splashStyle} from "../styles/styles";
+
+const splashImage = require('../assets/images/PokemonGqlSplashAlt.png')
 
 export const SplashScreen = () => {
-  return (
-    <View style={styles.root}>
-      <Text>Splash Screen</Text>
-      <Button title="Home" color="#710ce3" onPress={() => Navigation.setRoot(mainRoot)} />
+    return (
+    <View style={splashStyle.screenContainer}>
+        <PokedexHeader/>
+        <View style={introductionStyle.container}>
+            <Image source={splashImage} style={introductionStyle.splashImg}/>
+            {/*<Button title="LET'S START" style={introductionStyle.nextButton} onPress={() => Navigation.setRoot(mainRoot)} />*/}
+            <Pressable onPress={() => Navigation.setRoot(mainRoot)} style={introductionStyle.startButton}>
+                <Text style={introductionStyle.buttonText}>START NOW</Text>
+            </Pressable>
+        </View>
+        <View style={pokedexBottom.container}>
+            <View style={pokedexBottom.thingy}></View>
+        </View>
     </View>
-  );
+    );
 };
 
 SplashScreen.options = {
@@ -20,3 +32,48 @@ SplashScreen.options = {
     },
   },
 };
+
+const introductionStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 5,
+        // backgroundColor: 'skyblue', TODO
+    },
+    splashImg: {
+        height: 238,
+        width: 315,
+        resizeMode: "contain"
+    },
+    startButton: {
+        alignItems: "center",
+        backgroundColor: "#AC193C",
+        padding: 15,
+        borderRadius: 10,
+        // borderWidth: StyleSheet.hairlineWidth,
+        // borderColor: "white"
+    },
+    buttonText: {
+        fontFamily: "'WHITRABT', monospace",
+        fontWeight: "bold",
+        fontSize: 18,
+        color: "white",
+    }
+});
+
+const pokedexBottom = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        margin: 10,
+    },
+    thingy: {
+        height: 15,
+        width: 150,
+        backgroundColor: "#AC193C",
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: "black",
+        borderRadius: 20,
+    },
+})

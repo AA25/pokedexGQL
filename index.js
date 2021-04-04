e6da9287677615dc9136d4944848c53d2d080b76
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2021-04-04 11:04:56 +0100

    Deleted SettingScreen
    Redudant page

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ import { entryRoot } from "./src/navigation/nagivationRoots";
 import { Navigation } from "react-native-navigation";
 import { navigationOption } from "./src/navigation/navigationOptions";
 import { SearchScreen } from "./src/containers/SearchScreen";
-import { SettingsScreen } from "./src/containers/SettingScreen";
 import { SplashScreen } from "./src/containers/SplashScreen";
 
 Navigation.registerComponent("Home", () => SearchScreen);
-Navigation.registerComponent("Settings", () => SettingsScreen);
 Navigation.registerComponent("Splash", () => SplashScreen);
 
 Navigation.events().registerAppLaunchedListener(async () => {

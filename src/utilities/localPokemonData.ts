4d5c49e5a037669550331061d4a3a5c61ecf7316
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2021-04-04 18:16:37 +0100

    Added local pokemon data

---
+++ src/utilities/localPokemonData.ts
@@ -0,0 +1,62 @@
+export const localPokemonData = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    imageLocation: "https://i.pinimg.com/originals/3b/78/47/3b7847675982776e5219e12a680ecd84.png",
+    types: ["Grass"],
+  },
+  {
+    id: 7,
+    name: "Squirtle",
+    imageLocation: "http://assets.stickpng.com/images/580b57fcd9996e24bc43c32a.png",
+    types: ["Water"],
+  },
+  {
+    id: 100,
+    name: "Bulbasaur",
+    imageLocation: "https://i.pinimg.com/originals/3b/78/47/3b7847675982776e5219e12a680ecd84.png",
+    types: ["Grass"],
+  },
+  {
+    id: 101,
+    name: "Squirtle",
+    imageLocation: "http://assets.stickpng.com/images/580b57fcd9996e24bc43c32a.png",
+    types: ["Water"],
+  },
+  {
+    id: 102,
+    name: "Bulbasaur",
+    imageLocation: "https://i.pinimg.com/originals/3b/78/47/3b7847675982776e5219e12a680ecd84.png",
+    types: ["Grass"],
+  },
+  {
+    id: 103,
+    name: "Squirtle",
+    imageLocation: "http://assets.stickpng.com/images/580b57fcd9996e24bc43c32a.png",
+    types: ["Water"],
+  },
+  {
+    id: 104,
+    name: "Bulbasaur",
+    imageLocation: "https://i.pinimg.com/originals/3b/78/47/3b7847675982776e5219e12a680ecd84.png",
+    types: ["Grass"],
+  },
+  {
+    id: 105,
+    name: "Squirtle",
+    imageLocation: "http://assets.stickpng.com/images/580b57fcd9996e24bc43c32a.png",
+    types: ["Water"],
+  },
+  {
+    id: 106,
+    name: "Bulbasaur",
+    imageLocation: "https://i.pinimg.com/originals/3b/78/47/3b7847675982776e5219e12a680ecd84.png",
+    types: ["Grass"],
+  },
+  {
+    id: 107,
+    name: "Squirtle",
+    imageLocation: "http://assets.stickpng.com/images/580b57fcd9996e24bc43c32a.png",
+    types: ["Water"],
+  },
+];

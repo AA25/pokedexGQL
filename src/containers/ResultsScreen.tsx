32f3e88245c75480b0fac14ccd4541273960bdec
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2021-04-05 22:29:42 +0100

    Added apollo graphql
    Installed apollo graphql and integrated it to the Search root component.
    
    This change also includes creating ApolloClient via Singleton pattern. See class description for the reason behind this

--- src/containers/ResultsScreen.tsx
+++ src/containers/ResultsScreen.tsx
@@ -4,13 +4,19 @@ import { StyleSheet, View } from "react-native";
 import { appColours } from "../styles/colours";
 import { PokedexHeader } from "../components/PokedexHeader";
 import { SearchResult } from "../components/SearchResult";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClientSingleton } from "../utilities/ApolloClientSingleton";
+
+const client = new ApolloClientSingleton.getInstance();
 
 export const ResultsScreen = (props) => {
   return (
-    <View style={resultScreenStyle.screenContainer}>
-      <PokedexHeader />
-      <SearchResult componentId={props.componentId} />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={resultScreenStyle.screenContainer}>
+        <PokedexHeader />
+        <SearchResult componentId={props.componentId} />
+      </View>
+    </ApolloProvider>
   );
 };
 

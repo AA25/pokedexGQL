dbbc91d44c6c7edd0d42154f8c199df1e2a23d4f
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2021-04-07 19:11:31 +0100

    Created new component file
    This component file contains two components. PokemonList when provided with pokemon data will render a flat list of the data.
    
    PokemonListViaGraphQL when provided with additionalFields, will make a gql api request with the appropriate fields and pass that data to PokemonList for rendering.

---
+++ src/components/GraphqlPokemonData.tsx
@@ -0,0 +1,80 @@
+import { useQuery } from "@apollo/client";
+import { gql } from "@apollo/client/core";
+import { FlatList, SafeAreaView, StyleSheet, View } from "react-native";
+import { Loading } from "./Loading";
+import { FloatingBackButton } from "./FloatingBackButton";
+import { Error } from "./Error";
+import React from "react";
+import { Pokemon } from "./Pokemon";
+import PropTypes from "prop-types";
+
+export const PokemonListViaGraphQL = ({ componentId, allFields }) => {
+    const { data, loading, error } = useQuery(gql`
+    query {
+      pokemons {
+        ${allFields}
+      }
+    }
+  `);
+
+    if (loading) {
+        return (
+            <View style={styles.container}>
+                <Loading />
+                <FloatingBackButton componentId={componentId} navigationComponent={"Search"} />
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View style={styles.container}>
+                <Error />
+                <FloatingBackButton componentId={componentId} navigationComponent={"Search"} />
+            </View>
+        );
+    }
+
+    // TODO: transform layer here
+    const pokemonData = data.pokemons;
+
+    return <PokemonList componentId={componentId} pokemonData={pokemonData} />;
+};
+
+PokemonListViaGraphQL.propTypes = {
+    componentId: PropTypes.string,
+    allFields: PropTypes.string,
+};
+
+export const PokemonList = ({ componentId, pokemonData }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* List of Pokemon */}
+      <FlatList
+        data={pokemonData}
+        renderItem={({ item }) => (
+          <Pokemon id={item.id} name={item.name} imageSource={item.imageSource} type={item.type} />
+        )}
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+      />
+
+      {/* Back Button */}
+      <FloatingBackButton componentId={componentId} navigationComponent={"Search"} />
+    </SafeAreaView>
+  );
+};
+
+PokemonList.propTypes = {
+  componentId: PropTypes.string,
+  pokemonData: PropTypes.array,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+});

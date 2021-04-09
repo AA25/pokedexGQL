4835a150c201a9aa9a425bd6d0c96c2fcf38cc6c
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2021-04-09 08:51:00 +0100

    Updated component to pass api response to helper
    Because passing it to the helper check if it's empty, if so show No Result component

--- src/components/Results/GraphqlPokemonData.tsx
+++ src/components/Results/GraphqlPokemonData.tsx
@@ -7,6 +7,8 @@ import { Error } from "../Error";
 import React from "react";
 import { Pokemon } from "./Pokemon";
 import PropTypes from "prop-types";
+import { IPokemon, transformPokemonApiResult } from "../../helpers/TransformPokemonData";
+import { NoResult } from "../NoResult";
 
 export const PokemonListViaGraphQL = ({ componentId, allFields }) => {
   const { data, loading, error } = useQuery(gql`
@@ -35,8 +37,18 @@ export const PokemonListViaGraphQL = ({ componentId, allFields }) => {
     );
   }
 
-  // TODO: transform layer here
-  const pokemonData = data.pokemons;
+  // No data returned
+  if (data?.pokemons?.length === 0 || Object.keys(data.pokemons[0]).length === 0) {
+    return (
+      <View style={styles.container}>
+        <NoResult />
+        <FloatingBackButton componentId={componentId} navigationComponent={"Search"} />
+      </View>
+    );
+  }
+
+  // Transform response to ensure data as appropriate fields
+  const pokemonData: Array<IPokemon> = transformPokemonApiResult(data.pokemons);
 
   return <PokemonList componentId={componentId} pokemonData={pokemonData} />;
 };
@@ -53,7 +65,13 @@ export const PokemonList = ({ componentId, pokemonData }) => {
       <FlatList
         data={pokemonData}
         renderItem={({ item }) => (
-          <Pokemon id={item.id} name={item.name} imageSource={item.imageSource} type={item.type} />
+          <Pokemon
+            id={item.id}
+            name={item.name}
+            imageSource={item.imageSource}
+            type={item.type}
+            additionalData={item.additionalData}
+          />
         )}
         numColumns={2}
         keyExtractor={(item) => item.id}
